4454b756c8d75837e0682b354c335be71ce345a2
abhishekkarky <abhishekkarki40@gmail.com>
2024-08-17 13:50:44 +0545

    ssl

--- backend-31a-abhishekkarky/index.js
+++ backend-31a-abhishekkarky/index.js
@@ -1,48 +1,59 @@
-const express = require('express');
-
-const path = require('path');
-
-const cors = require('cors');
-
-const dotenv = require('dotenv');
-const connectDB = require('./database/db');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./database/db");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
 
 dotenv.config();
 
-app.use(express.json({ limit: '40mb' }))
-app.use(express.urlencoded({limit: '40mb', extended: true }));
+app.use(express.json({ limit: "40mb" }));
+app.use(express.urlencoded({ limit: "40mb", extended: true }));
 
 const corsPolicy = {
-    origin : true,
-    credentials : true,
-    optionSuccessStatus : 200,
-}
+  origin: true,
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 app.use(cors(corsPolicy));
 
-app.use('/uploads', (req, res, next) => {
-  express.static(path.resolve(__dirname, 'uploads'))(req, res, next);
+app.use("/uploads", (req, res, next) => {
+  express.static(path.resolve(__dirname, "uploads"))(req, res, next);
 });
 
 connectDB();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 443;
+
+// Load SSL certificate and key
+const sslOptions = {
+  key: fs.readFileSync(
+    path.resolve(__dirname, "localhost_ssl/managepoint.key")
+  ),
+  cert: fs.readFileSync(
+    path.resolve(__dirname, "localhost_ssl/managepoint.crt")
+  ),
+  // cert: fs.readFileSync(path.resolve(__dirname, 'path/to/your/server.crt'))
+};
 
 // creating user routes
-app.use('/api/user', require('./routes/userRoutes'))
+app.use("/api/user", require("./routes/userRoutes"));
 
 // creating subscriber routes
-app.use('/api/subscriber', require('./routes/subscriberRoutes'))
+app.use("/api/subscriber", require("./routes/subscriberRoutes"));
 
 // Creating group routes
-app.use('/api/group', require('./routes/groupRoutes'))
+app.use("/api/group", require("./routes/groupRoutes"));
 
 // Creating broadcast routes
-app.use('/api/broadcast', require('./routes/broadcastRoutes'))
+app.use("/api/broadcast", require("./routes/broadcastRoutes"));
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+// Create HTTPS server
+https.createServer(sslOptions, app).listen(port, () => {
+  console.log(`HTTPS Server is running on port ${port}`);
 });
 
 module.exports = app;
