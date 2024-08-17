3179f2250065443d9847c9e4e805b554496b6ff3
abhishekkarky <abhishekkarki40@gmail.com>
2024-08-17 18:42:18 +0545

    sanitize

--- backend-31a-abhishekkarky/model/groupModel.js
+++ backend-31a-abhishekkarky/model/groupModel.js
@@ -5,19 +5,22 @@ const groupSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
+      trim: true, // Remove leading and trailing spaces
     },
     subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: "subscriber" }],
     groupType: {
       type: String,
+      enum: ['type1', 'type2', 'type3'], // Replace with actual types
       required: true,
     },
     addedBy: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "users",
+      required: true, // Ensure that the user who adds the group is always recorded
     },
   },
   { timestamps: true }
 );
 
-const group = mongoose.model("group", groupSchema);
-module.exports = group;
+const Group = mongoose.model("group", groupSchema);
+module.exports = Group;
